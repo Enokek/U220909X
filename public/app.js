de9ad9ee67464c8394bfa3847ca889547c43b700
Enokek <enok.ekelund@gmail.com>
2022-09-12 14:30:02 +0200

    Finished the project and fixed remaining errors

--- public/app.js
+++ public/app.js
@@ -39,19 +39,22 @@ function build(){
     aboutEnok.appendChild(container);
 
     const aboutEnok1 = document.createElement("p") // Textruta 1 om mig själv
-    aboutEnok1.innerHTML = 'Hej jag hegete ajne ';
+    aboutEnok1.innerHTML = 'Hej jag heter Enok Ekelund och bor skogåsområdet. Jag brukar spela fotboll, träffa kompisar och spela' +
+    'datorspel på fritden. Just nu sparar jag till att köpa en egen bil och pluggar till körkortsteorin samtidigt. Min favorit mat är pizza.';
+    
     aboutEnok1.className = 'text-black w-50 float-start ';
     container.appendChild(aboutEnok1);
 
     const aboutEnok2 = document.createElement("p") // Textruta 2 om mig själv
-    aboutEnok2.innerHTML = 'Enok wakddwadawdwwan adwdwd  dwadw dadwad Enokwadd ddwwaewad ewadwadw  wwdwawad' +
-    'dw widwaoiwai iwadwa waiwaid idwad';
+    aboutEnok2.innerHTML = 'Hemma har jag en hund som heter Raffe som jag går ut med ungefär varannan dag och han bits inte, så ofta' +
+    'Utöver det så har jag en lillebror som jag bor vägg i vägg med och som är riktigt jobbig från tid till tid. Min favoritfärg är blålila.'
     aboutEnok2.className = 'text-black w-50 float-end ';
     container.appendChild(aboutEnok2);
 
     const aboutEnok3 = document.createElement("p") // Textruta 2 om mig själv
-    aboutEnok3.innerHTML = 'Duis pharetra quis sit amet condimentum eget condimentum vitae, sollicitudin commodo urna.' +
-    'Duis ultricies orci vel tortor luctus, sed ultricies odio suscipit.';
+    aboutEnok3.innerHTML = 'På sommaren jobbade jag på ICA i farsta och gick upp ungefär fem varje dag vilket sög. På tiden jag fick över brukade' +
+    'jag och några kompisar dra iväg och bada eller liknande. Framförallt var det flatenbadet som gällde men när vi inte orkade åka' +
+    'långt blev det istället drevviken, vilket är ett bad som ligger närmare där vi bor.';
     aboutEnok3.className = 'text-black w-100 text-center ps-5 ';
     aboutEnok.appendChild(aboutEnok3);
 
@@ -65,7 +68,20 @@ function build(){
     portraitTitle.className = 'text-black mt-3  ';
     imagePortraits.appendChild(portraitTitle);
 
-    
+    const img1 = document.createElement("img");
+    img1.className = 'w-25 h-25 me-3';
+    img1.setAttribute('src', 'enok.jpg')
+    imagePortraits.appendChild(img1);
+
+    const img2 = document.createElement("img");
+    img2.className = 'w-25 h-25';
+    img2.setAttribute('src', 'enok1.jpg')
+    imagePortraits.appendChild(img2);
+
+
+
+
+
 
   
     
